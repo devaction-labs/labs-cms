827db6aafb1f5ed563f31f12d11ed50fe167d912
Rafael Lunardelli <rafael@pinguim.academy>
2023-10-07 14:06:54 -0600

    crm-4: creating a token when asking to recover the password

--- tests/Feature/Auth/PasswordRecoveryTest.php
+++ tests/Feature/Auth/PasswordRecoveryTest.php
@@ -2,11 +2,11 @@
 
 use App\Livewire\Auth\Password\Recovery;
 use App\Models\User;
-use App\Notifications\PasswordRecoveryNotification;
+use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Support\Facades\Notification;
 use Livewire\Livewire;
 
-use function Pest\Laravel\get;
+use function Pest\Laravel\{assertDatabaseCount, assertDatabaseHas, get};
 
 test('needs to have a route to password recovery', function () {
     get(route('auth.password.recovery'))
@@ -27,7 +27,7 @@ it('should be able to request for a password recovery sending notification to th
 
     Notification::assertSentTo(
         $user,
-        PasswordRecoveryNotification::class
+        ResetPassword::class
     );
 });
 
@@ -40,3 +40,15 @@ test('testing email property', function ($value, $rule) {
     'required' => ['value' => '', 'rule' => 'required'],
     'email'    => ['value' => 'any email', 'rule' => 'email'],
 ]);
+
+test('needs to create a token when requesting for a password recovery', function () {
+    /** @var User $user */
+    $user = User::factory()->create();
+
+    Livewire::test(Recovery::class)
+        ->set('email', $user->email)
+        ->call('startPasswordRecovery');
+
+    assertDatabaseCount('password_reset_tokens', 1);
+    assertDatabaseHas('password_reset_tokens', ['email' => $user->email]);
+});
