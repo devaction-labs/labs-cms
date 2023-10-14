1b2b8cb7dd178144e7127104958841f1ad779935
Rafael Lunardelli <rafael@pinguim.academy>
2023-10-14 09:56:13 -0600

    crm-5: creating a seed for permissions

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,7 +13,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        $this->call([
+            PermissionSeeder::class,
+        ]);
 
         User::factory()->create([
             'name'  => 'Test User',
