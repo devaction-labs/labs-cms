52c3a966050e9c8e0fcc3e0c1ca5b418f32bd178
Rafael Lunardelli <rafael@lunardelli.me>
2023-10-21 11:40:20 -0600

    crm-12: setting up the front end of the table

--- tests/Feature/Admin/UserManagement/ListUsersTest.php
+++ tests/Feature/Admin/UserManagement/ListUsersTest.php
@@ -2,7 +2,6 @@
 
 use App\Livewire\Admin;
 use App\Models\User;
-use Illuminate\Pagination\LengthAwarePaginator;
 
 use function Pest\Laravel\{actingAs, get};
 
@@ -21,13 +20,13 @@ test('making sure that the route is protected by the permission BE_AN_ADMIN', fu
 });
 
 test("let's create a livewire component to list all users in the page", function () {
+    actingAs(User::factory()->admin()->create());
     $users = User::factory()->count(10)->create();
 
     $lw = Livewire::test(Admin\Users\Index::class);
     $lw->assertSet('users', function ($users) {
         expect($users)
-            ->toBeInstanceOf(LengthAwarePaginator::class)
-            ->toHaveCount(10);
+            ->toHaveCount(11);
 
         return true;
     });
@@ -35,4 +34,16 @@ test("let's create a livewire component to list all users in the page", function
     foreach ($users as $user) {
         $lw->assertSee($user->name);
     }
+});
+
+test('check the table format', function () {
+    actingAs(User::factory()->admin()->create());
+
+    Livewire::test(Admin\Users\Index::class)
+        ->assertSet('headers', [
+            ['key' => 'id', 'label' => '#'],
+            ['key' => 'name', 'label' => 'Name'],
+            ['key' => 'email', 'label' => 'Email'],
+            ['key' => 'permissions', 'label' => 'Permissions'],
+        ]);
 });
