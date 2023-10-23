dbe86239d624b255a2253ffe5c3d650a1e310e99
Rafael Lunardelli <rafael@lunardelli.me>
2023-10-23 17:05:25 -0600

    crm-12: frontend: showing arrows based on the sort rule

--- app/Livewire/Admin/Users/Index.php
+++ app/Livewire/Admin/Users/Index.php
@@ -67,7 +67,7 @@ class Index extends Component
             )
             ->when(
                 $this->search_trash,
-                fn (Builder $q) => $q->onlyTrashed() /** @phpstan-ignore-line */
+                fn (Builder $q) => $q->onlyTrashed()/** @phpstan-ignore-line */
             )
             ->orderBy($this->sortColumnBy, $this->sortDirection)
             ->get();
@@ -77,10 +77,10 @@ class Index extends Component
     public function headers(): array
     {
         return [
-            ['key' => 'id', 'label' => '#'],
-            ['key' => 'name', 'label' => 'Name'],
-            ['key' => 'email', 'label' => 'Email'],
-            ['key' => 'permissions', 'label' => 'Permissions'],
+            ['key' => 'id', 'label' => '#', 'sortColumnBy' => $this->sortColumnBy, 'sortDirection' => $this->sortDirection],
+            ['key' => 'name', 'label' => 'Name', 'sortColumnBy' => $this->sortColumnBy, 'sortDirection' => $this->sortDirection],
+            ['key' => 'email', 'label' => 'Email', 'sortColumnBy' => $this->sortColumnBy, 'sortDirection' => $this->sortDirection],
+            ['key' => 'permissions', 'label' => 'Permissions', 'sortColumnBy' => $this->sortColumnBy, 'sortDirection' => $this->sortDirection],
         ];
     }
 
@@ -90,5 +90,11 @@ class Index extends Component
             ->when($value, fn (Builder $q) => $q->where('key', 'like', "%$value%"))
             ->orderBy('key')
             ->get();
+    }
+
+    public function sortBy(string $column, string $direction): void
+    {
+        $this->sortColumnBy  = $column;
+        $this->sortDirection = $direction;
     }
 }
