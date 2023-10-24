141f2dea7c54b1f6a327fa1e4bfe4160c102d159
Rafael Lunardelli <rafael@lunardelli.me>
2023-10-23 21:35:23 -0600

    crm-14: backend: be able to delete an user

---
+++ tests/Feature/Admin/UserManagement/DeleteUserTest.php
@@ -0,0 +1,20 @@
+<?php
+
+use App\Livewire\Admin;
+use App\Models\User;
+
+use function Pest\Laravel\{actingAs, assertSoftDeleted};
+
+it('should be able to delete a user', function () {
+    $user        = User::factory()->admin()->create();
+    $forDeletion = User::factory()->create();
+
+    actingAs($user);
+    Livewire::test(Admin\Users\Delete::class, ['user' => $forDeletion])
+        ->call('destroy')
+        ->assertDispatched('user::deleted');
+
+    assertSoftDeleted('users', [
+        'id' => $forDeletion->id,
+    ]);
+});
