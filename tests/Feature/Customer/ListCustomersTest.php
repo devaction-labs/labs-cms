72f976c07c36927fcf85c2a164fdd4c5f315b942
Rafael Lunardelli <rafael@lunardelli.me>
2023-12-13 23:55:32 -0700

    cleaning tests

--- tests/Feature/Customer/ListCustomersTest.php
+++ tests/Feature/Customer/ListCustomersTest.php
@@ -2,7 +2,7 @@
 
 use App\Enum\Can;
 use App\Livewire\Customers;
-use App\Models\{Customer, Permission, User};
+use App\Models\{Customer, User};
 use Illuminate\Pagination\LengthAwarePaginator;
 
 use function Pest\Laravel\{actingAs, get};
@@ -66,48 +66,6 @@ it('should be able to filter by name and email', function () {
             expect($customers)
                 ->toHaveCount(1)
                 ->first()->name->toBe('Mario');
-
-            return true;
-        });
-});
-
-it('should be able to filter by permission.key', function () {
-    $admin       = User::factory()->admin()->create(['name' => 'Joe Doe', 'email' => 'admin@gmail.com']);
-    $nonAdmin    = User::factory()->withPermission(Can::TESTING)->create(['name' => 'Mario', 'email' => 'little_guy@gmail.com']);
-    $permission  = Permission::where('key', '=', Can::BE_AN_ADMIN->value)->first();
-    $permission2 = Permission::where('key', '=', Can::TESTING->value)->first();
-
-    actingAs($admin);
-    Livewire::test(Customers\Index::class)
-        ->assertSet('customers', function ($customers) {
-            expect($customers)->toHaveCount(2);
-
-            return true;
-        })
-        ->set('search_permissions', [$permission->id, $permission2->id])
-        ->assertSet('customers', function ($customers) {
-            expect($customers)
-                ->toHaveCount(2);
-
-            return true;
-        });
-});
-
-it('should be able to list deleted customers', function () {
-    $admin        = User::factory()->admin()->create(['name' => 'Joe Doe', 'email' => 'admin@gmail.com']);
-    $deletedUsers = User::factory()->count(2)->create(['deleted_at' => now()]);
-
-    actingAs($admin);
-    Livewire::test(Customers\Index::class)
-        ->assertSet('customers', function ($customers) {
-            expect($customers)->toHaveCount(1);
-
-            return true;
-        })
-        ->set('search_trash', true)
-        ->assertSet('customers', function ($customers) {
-            expect($customers)
-                ->toHaveCount(2);
 
             return true;
         });
