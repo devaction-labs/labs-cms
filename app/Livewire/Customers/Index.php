bd7076b1ee979ef7719ea3a340d8b81af4a70239
Rafael Lunardelli <rafael@lunardelli.me>
2024-01-26 10:18:57 -0700

    crm-20: telling the modal to close and telling the list to reload

--- app/Livewire/Customers/Index.php
+++ app/Livewire/Customers/Index.php
@@ -7,13 +7,14 @@ use App\Support\Table\Header;
 use App\Traits\Livewire\HasTable;
 use Illuminate\Contracts\View\View;
 use Illuminate\Database\Eloquent\Builder;
-use Livewire\{Component, WithPagination};
+use Livewire\{Attributes\On, Component, WithPagination};
 
 class Index extends Component
 {
     use WithPagination;
     use HasTable;
 
+    #[On('customer::reload')]
     public function render(): View
     {
         return view('livewire.customers.index');
