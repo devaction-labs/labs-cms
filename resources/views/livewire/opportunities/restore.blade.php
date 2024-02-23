01b53b3185db35e49c3803bc9b158b8a0b44dd45
Rafael Lunardelli <rafael@lunardelli.me>
2024-02-23 10:37:50 -0700

    crm-24: restore an opportunity

--- resources/views/livewire/opportunities/restore.blade.php
+++ resources/views/livewire/opportunities/restore.blade.php
@@ -1,3 +1,10 @@
-<div>
-    {{-- Do your work, then step back. --}}
-</div>
+<x-modal wire:model="modal"
+         title="Restore Confirmation"
+         subtitle="You are restoring the opportunity {{ $opportunity?->name }}"
+         separator>
+
+    <x-slot:actions>
+        <x-button label="Hum... no" @click="$wire.modal = false"/>
+        <x-button label="Yes, I am" class="btn-primary" wire:click="restore"/>
+    </x-slot:actions>
+</x-modal>
