c9c674ed61a38bc7bcdf19f0f75bcb4b900686ab
Alex Nogueira <alex@devaction.com.br>
2024-08-17 11:24:50 -0300

    refactor: improve email obfuscation and version retrieval logic

--- app/Support/HelperFunctions.php
+++ app/Support/HelperFunctions.php
@@ -2,25 +2,38 @@
 
 function obfuscate_email(?string $email = null): string
 {
-    if (!$email) {
+    if (is_null($email) || !strpos($email, '@')) {
         return '';
     }
 
-    $split = explode('@', $email);
+    [$firstPart, $secondPart] = explode('@', $email);
 
-    if(sizeof($split) != 2) {
-        return '';
-    }
+    $qty             = (int) floor(strlen($firstPart) * 0.75);
+    $remainingFirst  = strlen($firstPart) - $qty;
+    $remainingSecond = strlen($secondPart) - $qty;
 
-    $firstPart       = $split[0];
-    $qty             = (int)floor(strlen($firstPart) * 0.75);
-    $remaining       = strlen($firstPart) - $qty;
-    $maskedFirstPart = substr($firstPart, 0, $remaining) . str_repeat('*', $qty);
-
-    $secondPart       = $split[1];
-    $qty              = (int)floor(strlen($secondPart) * 0.75);
-    $remaining        = strlen($secondPart) - $qty;
-    $maskedSecondPart = str_repeat('*', $qty) . substr($secondPart, $remaining * -1, $remaining);
+    $maskedFirstPart  = substr($firstPart, 0, $remainingFirst) . str_repeat('*', $qty);
+    $maskedSecondPart = str_repeat('*', $qty) . substr($secondPart, $remainingSecond * -1, $remainingSecond);
 
     return $maskedFirstPart . '@' . $maskedSecondPart;
+}
+
+/**
+ * @throws JsonException
+ */
+function version(): string
+{
+    $composerJson = file_get_contents(base_path('composer.json'));
+
+    if ($composerJson === false) {
+        throw new RuntimeException('Failed to read composer.json');
+    }
+
+    $composerData = json_decode($composerJson, true, 512, JSON_THROW_ON_ERROR);
+
+    if (!is_array($composerData) || !isset($composerData['version'])) {
+        throw new RuntimeException('Version key not found in composer.json');
+    }
+
+    return 'Version:' . $composerData['version'];
 }
