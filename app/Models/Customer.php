f2092d4f1c5d4d38efafe7def3f6866bcd41ea5b
Alex Nogueira <alex@devaction.com.br>
2024-08-25 09:32:45 -0300

    refactor(Auth): streamline email validation logic and view

--- app/Models/Customer.php
+++ app/Models/Customer.php
@@ -6,6 +6,17 @@ use App\Traits\Models\HasSearch;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\{Model, SoftDeletes};
 
+/**
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property string $phone
+ * @property string $address
+ * @property string $city
+ * @property string $state
+ * @property string $zip
+ * @property string $country
+ */
 class Customer extends Model
 {
     use HasFactory;
--- resources/views/livewire/auth/email-validation.blade.php
+++ resources/views/livewire/auth/email-validation.blade.php
@@ -1,25 +1,31 @@
 <x-card title="{{ __('Enter your pin') }}" subtitle="{{ __('Copy and paste the pin here') }}" shadow separator
         class="flex items-center justify-center min-h-screen bg-neutral text-neutral-content">
     <div class="flex flex-col items-center justify-center mb-2">
-        <x-pin
-            wire:model.live="code"
-            size="6"
-            numeric
-            @completed="$wire.show = true"
-            @incomplete="$wire.show = false"
-        />
-        <template x-if="$wire.show">
-            @error('code')
-            <x-alert icon="o-x-circle" class="alert-danger alert mt-2 text-red-500 text-red-700">
-                <span class="text-sm text-red-500">{{ $message }}</span>
-                @enderror
-            </x-alert>
-        </template>
+        <div class="flex items-center">
+            <x-pin
+                wire:model.live="code"
+                size="6"
+                numeric
+                @completed="$wire.show = true"
+                @incomplete="$wire.show = false"
+            />
+            <template x-if="$wire.show">
+                <x-button tooltip="{{ __('Send Verify Code') }}" icon="o-check-circle"
+                          class="w-12 h-12 bg-primary text-white p-2 rounded-full ml-2"
+                          wire:click="handle" wire:loading.attr="disabled"
+                />
+            </template>
+        </div>
+        @error('code')
+        <x-alert icon="o-x-circle" class="alert-danger alert mt-2 text-red-500 text-red-700">
+            <span class="text-sm text-red-500">{{ $message }}</span>
+        </x-alert>
+        @enderror
+
         @if ($sendNewCodeMessage)
             <span class="text-green-900-500 text-sm text-green-900">{{ $sendNewCodeMessage }}</span>
         @endif
     </div>
-
     <form wire:submit="sendNewCode">
         <div class="flex justify-center">
             <button class="btn btn-primary btn-sm" type="submit" wire:loading.attr="disabled">
