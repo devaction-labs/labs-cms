3cfef8f617d4b21f12cd6763482ee470100a5f85
Alex Nogueira <alex@devaction.com.br>
2024-10-19 21:18:19 -0300

    test: add mock response for onboarding customer test

--- tests/Feature/Onboarding/CustomerOnboardingTest.php
+++ tests/Feature/Onboarding/CustomerOnboardingTest.php
@@ -1,11 +1,32 @@
 <?php
 
+use App\Http\Integrations\Anexia\Requests\CreateOnboardingRequest;
 use App\Livewire\Customers\Create;
 use Livewire\Livewire;
 
-use function Pest\Laravel\{assertDatabaseHas};
+use function Pest\Laravel\assertDatabaseHas;
+
+use Saloon\Http\Faking\MockResponse;
+use Saloon\Laravel\Facades\Saloon;
 
 test('check if the create method creates a new customer and does the onboarding', function () {
+    // Configurar o mock do Saloon com um array de mapeamento
+    Saloon::fake([
+        CreateOnboardingRequest::class => MockResponse::make([
+            'user' => [
+                'id'         => '01jak0p9ek6jmk8378htyc22kq',
+                'name'       => 'Alex Nogueira',
+                'email'      => 'alex@devaction.com.br',
+                'tenant_id'  => '01jak0p98dvn7sgcf89wg1acc0',
+                'created_at' => now()->toISOString(),
+                'updated_at' => now()->toISOString(),
+            ],
+            'event'   => 'company.registered',
+            'message' => 'Cliente criado com sucesso!', // Adicionando a chave "message"
+        ], 200), // 200 é o status HTTP de sucesso
+    ]);
+
+    // Agora execute o teste normalmente
     Livewire::test(Create::class)
         ->set('form.name', 'Alex Nogueira')
         ->set('form.email', 'alex@devaction.com.br')
